4d60714237d44ed7aca9e28856120c2584ed3d18
Jose Soto <jsoto7087@gmail.com>
2021-01-21 20:58:49 -0500

    commit tree 4a1faa2006caf97a3042603e274115a07429cb83

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,27 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+#  key_for_min_value = {key, value}
+#  key_for_min_value(name_hash)
+ #lowest_key == lowest_key |key, value|
+#  return name_hash
+low_key = nil
+low_value = nil
+name_hash.each do |key, value|
+  low_key = key if low_key == nil 
+  low_value = value if low_value == nil
+  if value < low_value
+      low_value = value
+      low_key = key
+  end
 end
+low_key
+end
+
+# ikea = {:chair => 25, :table => 85, :mattress => 450}
+# key_for_min_value(ikea)
+# # => :chair
+
+# veggies = {"apple" => -45, "banana" => -44.5, "carrot" => -44.9}
+# key_for_min_value(veggies)
+# # => "apple"
